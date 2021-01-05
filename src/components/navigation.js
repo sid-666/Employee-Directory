d044bd4f14041797284a880b5909d4b9b4c6b426
Seance <siddharth.dhananjay@gmail.com>
2021-01-05 11:41:28 +1100

    imported stylesheet as well as changed class names

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,12 +1,13 @@
-  
+
 import React from "react";
+import "../styles/navigation.css";
 
 function Navigation({ onSearch, searchTerm, handleSortByName, handleSortByDept }) { // The two props coming down from the Navigation Component
     return (
-        <div className="md:flex justify-between my-6">
-            <div className="md:mb-0 mb-4">
-                <button onClick={handleSortByName} className="bg-white focus:bg-yellow-500 focus:text-white focus:outline-none shadow font-bold text-gray-800 rounded px-8 py-1 mr-4 hover:bg-yellow-500 hover:text-white">Name</button>
-                <button onClick={handleSortByDept} className="bg-white focus:bg-yellow-500 focus:text-white focus:outline-none shadow font-bold text-gray-800 rounded px-8 py-1 hover:bg-yellow-500 hover:text-white">Dept</button>
+        <div className="nav">
+            <div className="category">
+                <button onClick={handleSortByName} className="name">Name</button>
+                <button onClick={handleSortByDept} className="department">Dept</button>
             </div>
             <form>
                 <input
@@ -14,7 +15,7 @@ function Navigation({ onSearch, searchTerm, handleSortByName, handleSortByDept }
                     onChange={onSearch} // the onChange is the event listening attribute that uses the onSearch prop method let state know there was a change
                     className="shadow pl-5 rounded w-full py-1"
                     type="text"
-                    placeholder="search employee"/>
+                    placeholder="search employee" />
             </form>
         </div>
     )
